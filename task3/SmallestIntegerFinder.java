719692aaa21fc7be61a3924446386bd2f15dc69b
_Sozdatel_ <132205456+Sozdatel2008@users.noreply.github.com>
2026-04-07 17:23:30 +0400

    First CodeWars task!

---
+++ task3/SmallestIntegerFinder.java
@@ -0,0 +1,24 @@
+package task3;
+
+// Given an array of integers your solution should find the smallest integer.
+// For example:
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+public class SmallestIntegerFinder {
+    public static int findSmallestInt(int[] args) {
+        int smallestInt = args[0];
+        for(int i = 0; i < args.length; i++){
+            if(args[i] < smallestInt){
+                smallestInt = args[i];
+            }
+        }
+        return smallestInt;
+    }
+  
+    public static void main(String[] args){
+        int[] numbers = {-3, 5, -1, -2};
+        System.out.println(findSmallestInt(numbers));
+    }
+}
