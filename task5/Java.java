a6718f5e2254173599461e3eb3689ffe3b549e59
_Sozdatel_ <132205456+Sozdatel2008@users.noreply.github.com>
2026-04-09 10:18:44 +0400

    Task5
    Return doubled number

---
+++ task5/Java.java
@@ -0,0 +1,18 @@
+package task5;
+
+//Code as fast as you can! You need to double the integer and return it.
+
+class Java {
+    public static int doubleInteger(int i) {
+        return i*2;
+    } 
+  
+    public static void main(String args[]){
+        int integer = 8; //example1
+        System.out.println(doubleInteger(integer));
+        integer = 25; //example2
+        System.out.println(doubleInteger(integer));
+        integer = 250; //example3
+        System.out.println(doubleInteger(integer));
+    } 
+}
